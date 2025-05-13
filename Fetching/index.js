b3d049fda0ee9476f186ce017867c0808fea8b14
GeekJulia <juliayaks148@gmail.com>
2025-05-13 12:41:17 +0100

    Got ID name

--- Fetching/index.js
+++ Fetching/index.js
@@ -1,5 +1,6 @@
 const fetchData = async () => {
     try{
+        const pokemonName = document.getElementById("pokemonName").ariaValueMax.toLowerCase();
         const response = await fetch("https://pokeapi.co/api/v2/pokemon/typhlosion");
         if(!response.ok){
             throw new Error("Could not fetch data");
