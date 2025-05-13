6cd8edf6214b124fe35c13ae6acaa7506ec17e4b
GeekJulia <juliayaks148@gmail.com>
2025-05-13 12:05:41 +0100

    Fetching with async

--- Fetching/index.js
+++ Fetching/index.js
@@ -1,8 +1,9 @@
 const fetchData = async () => {
     try{
-
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/typhlosion");
+        
     }
     catch(error){
-
+        console.error(error.status)
     }
 }
