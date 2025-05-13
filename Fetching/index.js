9afb1258dd8bd8d49dd3ae62e101752de56d66fc
GeekJulia <juliayaks148@gmail.com>
2025-05-13 12:10:47 +0100

    throw new error, reassign data

--- Fetching/index.js
+++ Fetching/index.js
@@ -1,9 +1,15 @@
 const fetchData = async () => {
     try{
         const response = await fetch("https://pokeapi.co/api/v2/pokemon/typhlosion");
-        
+        if(!response.ok){
+            throw new Error("Could not fetch data");
+        }
+        const data = await response.json();
+        console.log(data);
     }
     catch(error){
         console.error(error.status)
     }
 }
+
+fetchData();
